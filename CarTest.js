4acf3d20cffd2d02ad4f2591b9052c5206fb7412
Elise Carmichael <ecarmichael@mobiquityinc.com>
2014-02-13 11:33:11 -0500

    added basic exercise 1

--- CarTest.js
+++ CarTest.js
@@ -1,57 +1,24 @@
 var car = require("./Car.js"),
-	vows = require('vows'),
-	assert = require('assert');
-	
-vows.describe('Car').addBatch({
-	'when displaying the car\'s name' : {
-		topic : car,
-		'it should contain the make, model and year' : function(testCar) {
-			assert.equal (testCar.getName(), testCar.year + " " + testCar.make + " " + testCar.model);
-		}
-	},
-    'when starting a car built before 1985': {
-        topic : car,
-        'we get a chugga with a vroom': function(testCar) {
-			testCar.year = 1970;
-            assert.equal (testCar.start(), "chugga chugga vroom vroom!");
-        }
-    },
-    'when starting a car built after 1985': {
-        topic : car,
-        'we get a nice and healthy sounding vroom vroom': function(testCar) {
-			testCar.year = 1997;
-         	assert.equal (testCar.start(), "vroom vroom!");
-        }
-    },
-	'A car' : {
-		topic : car,
-		'should be able to fly' : function(testCar) {
-			assert.isDefined(testCar.canFly);
-		},
-		'should be able to be a convertable' : function(testCar) {
-			assert.isDefined(testCar.isConvertable);
-		}
-	},
-	'If a car can fly' : {
-		topic : car,
-		'it should also know its altitude' : function(testCar) {
-			if (testCar.canFly) {
-				assert.isDefined(testCar.altitude);
-			} 
-		}
-	},
-	'If a car is a converable' : {
-		topic : car,
-		'it should be able to put its top up' : function(testCar) {
-			if (testCar.isConvertable) {
-				assert.equal(testCar.putTopUp(), true);
-			} 
-		},
-		'it should be able to put its top down' : function(testCar) {
-			if (testCar.isConvertable) {
-				assert.equal(testCar.putTopDown(), false);
-			} 
-		}
-	}
+    assert = require('assert'),
+    should = require('should');
 
-}).run();
+describe('Car', function() { 
+    var testCar = car;
+    describe('when displaying the car\'s name', function() {
+        it('should contain the make, model and year', function() {
+            assert.equal(testCar.getName(), testCar.year + " " + testCar.make + " " + testCar.model);
+        })
+    })
+    describe('when start a car built before 1985', function() {
+        it('should give a chugga with a vroom', function() {
+            testCar.year = 1970;
+            should.equal(testCar.start(), "chugga chugga vroom vroom!");
+        })
+    })
+    describe('when starting a car built after 1985', function() {
+        it('should give a chugga with a vroom', function() {
+            testCar.year = 1997;
+            assert.equal(testCar.start(), "vroom vroom!");
+        })
+    })
+});
